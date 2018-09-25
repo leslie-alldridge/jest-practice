2e1df14ba9f0fa324e1d827dc94914a0e3c4e765
leslie-alldridge <leslie.alldridge@gmail.com>
2018-09-25 21:35:56 +1200

    basic tests done

--- functions.js
+++ functions.js
@@ -1,6 +1,17 @@
+const axios = require('axios')
+
 const functions = {
   add: (num1, num2) => num1 + num2,
-  isNull: () => null
+  isNull: () => null,
+  checkValue: (x) => x,
+  createUser: () => {
+    const user = {firstname: 'leslie'}
+    user['lastname'] = 'alldridge';
+    return user;
+  },
+  fetchUser: () => axios.get('https://jsonplaceholder.typicode.com/todos/1')
+  .then(res => res.data)
+  .catch(err => 'error')
 };
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -9,5 +9,39 @@ test("adds 2 + 2 to not equal", () => {
 });
 
 test("should be null", () => {
-  expect(functions.add(2, 3)).not.toBe(2);
+  expect(functions.isNull()).toBeNull()
 });
+
+test("should be the falsey", () => {
+  expect(functions.checkValue(null)).toBeFalsy()
+});
+
+test("should be our user", () => {
+  expect(functions.createUser()).toMatchObject({
+    firstname: 'leslie',
+    lastname: 'alldridge'
+  })
+});
+
+test("should be under load 1600", () => {
+  const load1 = 800
+  const load2 = 500
+  expect(load1+load2).toBeLessThanOrEqual(1600)
+});
+
+test("there is no i in team", () => {
+  expect('team').not.toMatch(/I/i);
+});
+
+test("admin should be a user", () => {
+  const usernames = ['leslie', 'yusuki', 'admin']
+  expect(usernames).toContain('admin')
+}); 
+
+test("async data", () => {
+  expect.assertions(1);
+  return functions.fetchUser()
+  .then(data => {
+    expect(data.title).toEqual('delectus aut autem')
+  })
+}); 
